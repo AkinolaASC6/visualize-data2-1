727f87f636258ab78e3315eae1ac3921b454cc59
ndhbASC6 <nicolas.hughesbarrow0@gmail.com>
2019-08-02 14:31:19 -0400

    final

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # visualize-data2
 Project
 
-1. Get the data object from the browser
-2. Copy the data into a data.js file
-3. Setup index.html, main.js, style.css and make sure they are  properly linked to eachother
-4. Story: I'm in a poker club and their website should list the new members (pictures, names, some information)
-5. MVP: title (New Members), one card with pictures + name(with proper capitalization) + dob
+1. Get the data object from the browser [COMPLETE]
+2. Copy the data into a data.js file [COMPLETE]
+3. Setup index.html, main.js, style.css and make sure they are  properly linked to eachother [COMPLETE]
+4. Story: We are Goldman Sachs and need an index to track the information of all of our employees (pictures, names, some information) {IN PROGRESS}
+5. MVP: title (New Members), one card with pictures + name(with proper capitalization) + dob {IN PROGRESS}
 6. Use CSS to make it look pretty
 7. Other features(buttons, landing page etc.)
 8. Deploy using GitHub
--- src/main.js
+++ src/main.js
@@ -1,19 +1,40 @@
-// const first = list[0];
 const parent  = document.querySelector(".main-element")
 const createCard = (obj, indx) => {
     const newCard = document.createElement("div");
     newCard.className = "card";
     newCard.id = indx;
 
+    const infoBox = document.createElement("div");
+    infoBox.className = "info";
+
     const face = document.createElement("img");
     face.src = obj.picture.medium;
     face.alt = "portrait";
 
-    newCard.appendChild(face);
-    parent.appendChild(newCard);
+    const name = document.createElement("p");
+    let nameFormat = properCap(obj.name.title) + ". "  + properCap(obj.name.first) + " " + properCap(obj.name.last);
+    setProperties(name, "name", nameFormat);
 
-    console.log(obj.gender);
-};
+    const email = document.createElement("p");
+    setProperties(email, "email", obj.email)
+
+    const phone = document.createElement("p");
+    setProperties(phone, "phone", obj.phone);
+
+    const city = document.createElement("p");
+    setProperties(city, "city", properCap(obj.location.city));
+    
+    let info = [];
+    info.push(name, email, phone, city);
+    for(fact of info) {
+        infoBox.appendChild(fact);
+    }
+    newCard.appendChild(face);
+    newCard.appendChild(infoBox);
+    parent.appendChild(newCard);
+    
+    console.log(info);
+}
 const createAll = arr => {
     for(let i = 0; i < arr.length; i++) {
         createCard(arr[i], i);
@@ -21,5 +42,9 @@ const createAll = arr => {
 }
 const properCap = str => {
     return str.replace(str[0], str[0].toUpperCase());
+}
+const setProperties = (elmt, call, text) => {
+    elmt.id = call;
+    elmt.innerHTML = text;
 }
 createAll(list);
